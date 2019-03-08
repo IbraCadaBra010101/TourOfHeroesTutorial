632064a00f19bf6d0f7179fa03af12c9ff87f3e8
IbraCadaBra010101 <44027882+IbraCadaBra010101@users.noreply.github.com>
2019-03-08 13:10:29 +0100

    tutorial- first and only commit.

---
+++ app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Not Narco'},
+  {id: 14, name: 'Make Angular Great Again'},
+  {id: 15, name: 'Celerias'},
+  {id: 16, name: 'Magneta'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
+
